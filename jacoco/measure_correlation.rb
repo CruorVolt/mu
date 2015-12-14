9abfb2877e01278ea2eb02748b83df2b1d4a614d
Anders Lundgren <lundgren.am@gmail.com>
2015-12-14 13:40:29 -0700

    Where?

--- jacoco/measure_correlation.rb
+++ jacoco/measure_correlation.rb
@@ -33,35 +33,36 @@ def parse_file(file)
                         live = true
                         parts = new_line.split /[{}]/
                         if parts[0] == "" then next end #erroneous unlabled result
-                        method_name = get_mutant_method(parts[0].strip.split(/[ &]/)[0], class_name)
                         mutant_label = parts[0].split(/&/)[0].lstrip #full mutant name
-                        mutants[mutant_label = {} unless mutants.has_key? mutant_label
+                        mutants[mutant_label] = {:live=>0, :dead=>0} unless mutants.has_key? mutant_label 
                         exceptions = parts[0].split /&/
-                        exception_map = {}
-                        except = false
-                        if exceptions.length != 1 #there were exceptions
+                        #exception_map = {} #Let's just ignore exceptions for now
+                        #except = false
+                        #if exceptions.length != 1 #there were exceptions
                             #each item an exception
-                            for index in 1..(exceptions.length-1) do
-                                exc_parts = exceptions[index].split /\./
-                                if !(exception_map.has_key? exc_parts[1]) #func
-                                    exception_map[exc_parts[1]] = [ exc_parts[2] ]
-                                else
-                                    exception_map[exc_parts[1]] << exc_parts[2]
-                                end
-                            end
-                        end
+                            #for index in 1..(exceptions.length-1) do
+                                #exc_parts = exceptions[index].split /\./
+                                #if !(exception_map.has_key? exc_parts[1]) #func
+                                    #exception_map[exc_parts[1]] = [ exc_parts[2] ]
+                                #else
+                                    #exception_map[exc_parts[1]] << exc_parts[2]
+                                #end
+                            #end
+                        #end
                         
                         if parts.length == 1 #timeout
                             #live = false #timeouts always die
-                            timeout += 1
+                            #timeout += 1
                             next #ignore timeouts
-                        elsif exception_map.has_key? method_name
+                        #elsif exception_map.has_key? method_name
                             #print "Exception_map has key - #{method_name}: #{exception_map[method_name]}\n"
-                            if (exception_map[method_name].include? @mr) or (@mr == "aggregate")
+                            #if (exception_map[method_name].include? @mr) or (@mr == "aggregate")
                                 #print "^^adding\n"
-                                subtle += 1
-                            end
-                            next #ingnore exceptions
+                                #subtle += 1
+                            #end
+                            #next #ingnore exceptions
+                        elsif exceptions.length > 1
+                            next
                         else
                             tests = parts[1].split /,/
                             live = true
@@ -71,56 +72,49 @@ def parse_file(file)
                                 if (!live) then break end
                             end
                         end
-                        if mutants[mutant_label].has_key? "live"
+                        if live
+                            mutants[mutant_label][:live] += 1
+                        else
+                            mutants[mutant_label][:dead] += 1
+                        end
 
                     end
                     #this line done
 
                 end
 
-                @mutant_types.each do |type|
-
-                    if live_map.has_key? type
-                        write_line << live_map[type]
-                        total_live += live_map[type]
-                    else
-                        write_line << 0
-                    end
-
-                    if killed_map.has_key? type
-                        write_line << killed_map[type]
-                        total_killed += killed_map[type]
-                    else
-                        write_line << 0
-                    end
-                end
-                #write totals
-                write_line << timeout
-                write_line << subtle
-                write_line << total_live                
-                write_line << total_killed                
-                write_line << (total_killed.to_f) / subtle
-                mutant_score = ((total_killed.to_f) / (total_killed + total_live))
-                write_line << "%.2f" % mutant_score
                 run_count += 1
                 #print "Subtles:  #{subtle}\n"
                 #print "Timeouts: #{timeout}\n"
             end
         end
     rescue EOFError
-        #print "next File!\n"
+        print "next File!\n"
     ensure 
     end
-
+    mutants
 end
 
-#mr_file = File.open("report_MethodCollection2_mrs", "r")
 mr_map = parse_file("report_MethodCollection2_mrs")
+
+mr_map.keys.each do |mut|
+    puts "#{mut}: #{mr_map[mut]}"
+end
+=begin
 pre_map = build_map("report_MethodCollection2_pre.csv")
 post_map = build_map("report_MethodCollection2_post.csv")
 
-#pre_map.keys.each do |key|
-    #puts key
-#end
+match = 0
+mismatch = 0
+pre_map.keys.each do |key|
+    if pre_map[key] == post_map[key]
+        match += 1
+    else
+        mismatch += 1
+    end
+end
 
-#mr_file.close
+print "Match: #{match}"
+print "Mismatch: #{mismatch}"
+
+=end
